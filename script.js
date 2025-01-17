2d1815d7045beb3848b88872c6b07b1d0627ed18
Skushal-design <skushal583@gmail.com>
2025-01-17 07:54:55 +0545

    This is my bootstrap project to git

---
+++ script.js
@@ -0,0 +1,54 @@
+// Navbar scroll
+
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbarNav").style.top = "-100px";
+  } else {
+    document.getElementById("navbarNav").style.top = "0";
+  }
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const typewriterText = document.querySelector('.typewriter-text');
+  typewriterText.style.animationDelay = '0.5s';
+});
+
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
